8c43c33866c9ccbcb2307aeae8e5575a54e1985e
kewang <cpckewang@gmail.com>
2016-07-05 16:19:52 +0800

    Use start() to init variable

--- src/main/java/tw/kewang/redmine/appender/RedmineAppender.java
+++ src/main/java/tw/kewang/redmine/appender/RedmineAppender.java
@@ -1,6 +1,5 @@
 package tw.kewang.redmine.appender;
 
-import ch.qos.logback.classic.LoggerContext;
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.AppenderBase;
 import com.taskadapter.redmineapi.IssueManager;
@@ -9,41 +8,39 @@ import com.taskadapter.redmineapi.RedmineManager;
 import com.taskadapter.redmineapi.RedmineManagerFactory;
 import com.taskadapter.redmineapi.bean.Issue;
 import com.taskadapter.redmineapi.bean.IssueFactory;
-import org.slf4j.LoggerFactory;
 
 public class RedmineAppender extends AppenderBase<ILoggingEvent> {
     private String url;
     private String apiKey;
-    private LoggerContext context;
     private RedmineManager redmineManager;
     private IssueManager issueManager;
 
     @Override
+    public void start() {
+        String propertyRedmineUrl = getContext().getProperty("REDMINE_URL");
+        String propertyRedmineApiKey = getContext().getProperty("REDMINE_API_KEY");
+
+        if (propertyRedmineUrl == null || propertyRedmineUrl.length() == 0 || propertyRedmineApiKey == null
+                || propertyRedmineApiKey.length() == 0) {
+            System.out.println("No set REDMINE_URL / REDMINE_API_KEY");
+
+            return;
+        }
+
+        url = propertyRedmineUrl;
+        apiKey = propertyRedmineApiKey;
+        redmineManager = RedmineManagerFactory.createWithApiKey(url, apiKey);
+        issueManager = redmineManager.getIssueManager();
+
+        super.start();
+    }
+
+    @Override
     public void append(ILoggingEvent event) {
-        if (context == null) {
-            context = (LoggerContext) LoggerFactory.getILoggerFactory();
-        }
-
-        if (url == null || url.length() == 0) {
-            url = context.getProperty("REDMINE_URL");
-        }
-
-        if (apiKey == null || apiKey.length() == 0) {
-            apiKey = context.getProperty("REDMINE_API_KEY");
-        }
-
         createIssue(event);
     }
 
     private void createIssue(ILoggingEvent event) {
-        if (redmineManager == null) {
-            redmineManager = RedmineManagerFactory.createWithApiKey(url, apiKey);
-        }
-
-        if (issueManager == null) {
-            issueManager = redmineManager.getIssueManager();
-        }
-
         Issue issue = IssueFactory.create(22, event.getLoggerName());
 
         issue.setDescription(event.getFormattedMessage());
