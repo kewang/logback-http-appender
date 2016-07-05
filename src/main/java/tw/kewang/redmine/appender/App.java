b1867891adeb8fa48d35d2309091ee0575fbfd4f
kewang <cpckewang@gmail.com>
2016-07-05 12:45:35 +0800

    Add logback support

--- src/main/java/tw/kewang/redmine/appender/App.java
+++ src/main/java/tw/kewang/redmine/appender/App.java
@@ -1,13 +1,12 @@
 package tw.kewang.redmine.appender;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class App {
+    private static final Logger LOG = LoggerFactory.getLogger(App.class);
+
+    public static void main(String[] args) {
+        LOG.info("Hello World!");
     }
 }
